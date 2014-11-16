710574da68ccd7d0eb9e2ad9e87e0e7bf3936ace
callinterpol <oleg.zuravliov@gmail.com>
2014-11-16 23:39:07 +0200

    initial commit

---
+++ init.el
@@ -0,0 +1,51 @@
+(require 'package)
+(package-initialize)
+(push '("marmalade" . "http://marmalade-repo.org/packages/")
+      package-archives)
+(push '("melpa" . "http://melpa.milkbox.net/packages/")
+      package-archives)
+
+(require 'ido)
+(ido-mode t)
+
+(require 'evil)
+(evil-mode 1)
+
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+(load-theme 'zenburn t)
+
+(tool-bar-mode -1)
+
+(setq evil-normal-state-cursor '("red" hbar))
+(setq evil-insert-state-cursor '("green" bar))
+
+;;messagging buffer
+(setq message-log-max t)
+
+;;highlightning
+(global-hl-line-mode 1)
+(show-paren-mode 1)
+
+;;;;setup buffers
+;;remove scratch buffer
+(setq initial-scratch-message "")
+(defun remove-scratch-buffer ()
+  (if (get-buffer "*scratch*")
+      (kill-buffer "*scratch*")))
+
+(add-hook 'after-change-major-mode-hook 'remove-scratch-buffer)
+
+(setq inhibit-startup-buffer-menu t)
+
+;;remove startup screen
+(setq inhibit-startup-message t)
+
+(add-hook 'window-setup-hook 'delete-other-windows)
+
+;;avoid typing full yes or no 
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
